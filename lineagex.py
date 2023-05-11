196c45737efc9a07bc2fd0446fe66dcaf436c994
zshandy <zshandy@gmail.com>
2023-05-10 20:39:32 -0700

    Update lineagex.py

--- lineagex.py
+++ lineagex.py
@@ -14,7 +14,9 @@ class lineagex:
             password: Optional[str] = None,
     ) -> None:
         if sql is None:
-            raise ValueError("the SQL input cannot be empty, please input a list of sql or path")
+            raise ValueError("the SQL input cannot be empty, please input a list of sql or path to sql")
+        elif not isinstance(sql, list) and not isinstance(sql, str):
+            raise ValueError("wrong SQL input format, please input a list of sql or path to sql")
         if url and username and password:
             lx = LineageXWithConn(sql, search_schema, url, username, password)
             self.output_dict = lx.output_dict
