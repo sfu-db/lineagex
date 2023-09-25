d93434fd2c95052507d5198eb87486aaebee7b34
Aleksey Ovcharenko <938082+ovcharenko@users.noreply.github.com>
2023-09-25 14:13:20 +0300

    [MOD] Reformat with `black`

--- lineagex/utils.py
+++ lineagex/utils.py
@@ -27,7 +27,9 @@ def remove_comments(str1: Optional[str] = "") -> str:
 
 
 def find_column(
-    table_name: Optional[str] = "", engine: Any = None, search_schema: Optional[str] = ""
+    table_name: Optional[str] = "",
+    engine: Any = None,
+    search_schema: Optional[str] = "",
 ) -> List:
     """
     Find the columns for the base table in the database
@@ -113,7 +115,9 @@ def find_select(q: Optional[str] = "") -> str:
 
 
 def produce_json(
-    output_dict: Optional[dict] = None, engine: connection = None, search_schema: Optional[str] = ""
+    output_dict: Optional[dict] = None,
+    engine: connection = None,
+    search_schema: Optional[str] = "",
 ) -> dict:
     """
     Product the output.json and put into the html
@@ -125,7 +129,7 @@ def produce_json(
     # Get all the table names that are not in the output_dict(mostly base tables)
     all_tables = []
     for key, val in output_dict.items():
-        all_tables.extend(val['tables'])
+        all_tables.extend(val["tables"])
     all_tables = list(set(all_tables) - set(output_dict.keys()))
     base_table_dict = {}
     # If no conn is provided, try to guess the base table's columns
@@ -140,9 +144,13 @@ def produce_json(
         # if db conn is provided
         if engine and search_schema:
             if t.endswith("_ANALYZED"):
-                cols = find_column(table_name=t[:-9], engine=engine, search_schema=search_schema)
+                cols = find_column(
+                    table_name=t[:-9], engine=engine, search_schema=search_schema
+                )
             else:
-                cols = find_column(table_name=t, engine=engine, search_schema=search_schema)
+                cols = find_column(
+                    table_name=t, engine=engine, search_schema=search_schema
+                )
         else:
             cols = base_table_noconn_dict.get(t, [])
         for i in cols:
@@ -167,10 +175,10 @@ def _guess_base_table(output_dict: Optional[dict] = None) -> dict:
             for t in col_val:
                 idx = t.rfind(".")
                 if t[:idx] in base_table_noconn_dict.keys():
-                    if t[idx + 1:] not in base_table_noconn_dict[t[:idx]]:
-                        base_table_noconn_dict[t[:idx]].append(t[idx + 1:])
+                    if t[idx + 1 :] not in base_table_noconn_dict[t[:idx]]:
+                        base_table_noconn_dict[t[:idx]].append(t[idx + 1 :])
                 else:
-                    base_table_noconn_dict[t[:idx]] = [t[idx + 1:]]
+                    base_table_noconn_dict[t[:idx]] = [t[idx + 1 :]]
     return base_table_noconn_dict
 
 
